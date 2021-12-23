8f9725d0ec09fb7c88808bff7e688f6dd5194b94
Ameya <46456931+ameyalaad@users.noreply.github.com>
2021-12-23 15:28:01 +0530

    Update basic.sh
    Added base user, fixed grub install

--- arch/basic.sh
+++ arch/basic.sh
@@ -22,7 +22,7 @@ wait () {
 
 # Selecting a kernel to install (function). 
 kernel_selector () {
-    print "Installing Linux Zen kernel: A Linux kernel optimized for desktop usage"
+    print "Installing Linux zen kernel: A Linux kernel optimized for desktop usage"
     kernel="linux-zen"
 }
 
@@ -60,8 +60,6 @@ parted -s "$DISK" \
     mkpart primary ext4 1MiB 100% \
     set 1 boot on
 
-wait
-
 BOOT="/dev/sda1"
 # Formatting the ESP as FAT32.
 print "Formatting the $BOOT partition as ext4."
@@ -71,16 +69,12 @@ mkfs.ext4 -F $BOOT &>/dev/null
 print "Mounting the newly created volume."
 mount $BOOT /mnt
 
-wait 
-
 # Setting up the kernel.
 kernel_selector
 
 # Pacstrap (setting up a base sytem onto the new root).
 print "Installing the base system (it may take a while)."
-pacstrap /mnt base $kernel base-devel nano
-
-wait
+pacstrap /mnt base $kernel base-devel nano grub
 
 # Setting up the network.
 network_selector
@@ -100,7 +94,7 @@ print "Setting hosts file."
 cat > /mnt/etc/hosts <<EOF
 127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   $hostname.localdomain   $hostname
+127.0.1.1   $hostname
 EOF
 
 
@@ -117,18 +111,28 @@ arch-chroot /mnt /bin/bash -e <<EOF
     
     # Installing GRUB.
     echo "Installing GRUB on /boot."
-    pacman -S grub --needed &>/dev/null
-    grub-install /dev/sda &>/dev/null
+    grub-install /dev/sda 
 
     # Creating grub config file.
     echo "Creating GRUB config file."
-    grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
-
+    grub-mkconfig -o /boot/grub/grub.cfg 
+    
 EOF
 
 # Setting root password.
 print "Setting root password."
 arch-chroot /mnt /bin/passwd
+
+read -r -p "Please enter name for a user account (enter empty to not create one): " username
+
+# Setting user password.
+if [ -n "$username" ]; then
+    print "Adding the user $username to the system with root privilege."
+    arch-chroot /mnt useradd -m -G wheel -s /bin/bash "$username"
+    sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /mnt/etc/sudoers
+    print "Setting user password for $username." 
+    arch-chroot /mnt /bin/passwd "$username"
+fi
 
 # Finishing up.
 print "Done, you may now wish to reboot (further changes can be done by chrooting into /mnt)."
